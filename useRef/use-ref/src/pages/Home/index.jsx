c1216d29a24f3847c84acf97cdac8620379f5b68
RailJannatov <railcennetov@gmail.com>
2025-04-23 21:06:19 +0400

    react-hook-form and yup 2

--- useRef/use-ref/src/pages/Home/index.jsx
+++ useRef/use-ref/src/pages/Home/index.jsx
@@ -1,3 +1,37 @@
+import { useEffect, useState } from 'react';
+
 export const Home = () => {
-  return <div className="test">This is Home</div>;
+  const [isVisible, setIsVisible] = useState(true);
+  console.log('1');
+
+  useEffect(() => {
+    console.log('parent');
+    return () => {
+      console.log('parent cleanup');
+    };
+  }, []);
+
+  console.log('0');
+
+  return (
+    <>
+      <button onClick={() => setIsVisible(false)}>Click</button>
+      {isVisible && <Child />}
+    </>
+  );
+};
+
+const Child = () => {
+  console.log('5');
+
+  useEffect(() => {
+    console.log('child');
+    return () => {
+      console.log('child cleanup');
+    };
+  }, []);
+
+  console.log('-1');
+
+  return <div>child</div>;
 };
